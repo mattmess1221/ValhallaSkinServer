4b64389425ae9f19f4cd14daa07735037e7c1497
Matthew Messinger <mattmess1221@gmail.com>
2018-07-13 21:50:28 -0400

    Rebrand to valhalla

--- debug.sh
+++ debug.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-export FLASK_APP=hdskins/hdskins.py
+export FLASK_APP=valhalla/valhalla.py
 export FLASK_DEBUG=1
 
-export DB_FILE=hdskins.db
+export DB_FILE=valhalla.db
 export ROOT_DIR=$(pwd)
 export ROOT_URL=http://127.0.0.1:5000
 
