ab6ee3c21c52972e32a75a27cf54dd4048d34351
Matthew Messinger <mattmess1221@gmail.com>
2022-10-30 11:52:45 -0400

    Update mojang.py with pydantic settings

--- valhalla/mojang.py
+++ valhalla/mojang.py
@@ -2,7 +2,8 @@ from uuid import UUID
 
 import httpx
 from fastapi import HTTPException
-from pydantic import BaseModel
+
+from .schemas import BaseModel
 
 # ?username=username&serverId=hash&ip=ip"
 _VALIDATE = "https://sessionserver.mojang.com/session/minecraft/hasJoined"
@@ -12,9 +13,6 @@ class HasJoinedRequest(BaseModel):
     username: str
     server_id: str
     ip: str
-
-    def dict(self):
-        return {snake_to_camel(k): v for k, v in super().dict().items()}
 
 
 class HasJoinedResponse(BaseModel):
@@ -30,11 +28,5 @@ async def has_joined(params: HasJoinedRequest) -> HasJoinedResponse:
     async with httpx.AsyncClient() as client:
         response = await client.get(_VALIDATE, params=params.dict())
         if response.is_success:
-            return HasJoinedResponse.parse_obj(response.json)
-        raise HTTPException(500)
-
-
-def snake_to_camel(s: str) -> str:
-    words = s.split("_")
-    words[1:] = [w.capitalize() for w in words[1:]]
-    return "".join(words)
+            return HasJoinedResponse.parse_obj(response.json())
+        raise HTTPException(401)
