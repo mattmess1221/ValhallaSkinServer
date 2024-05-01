1e5324500246ce630008123ee7d9aad60bb81f00
Matthew Messinger <mattmess1221@gmail.com>
2024-05-01 12:02:47 -0400

    Use a standard error response for RateLimitExceeded

--- valhalla/limit.py
+++ valhalla/limit.py
@@ -1,9 +1,27 @@
-from fastapi import FastAPI
-from slowapi import Limiter, _rate_limit_exceeded_handler
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import JSONResponse
+from slowapi import Limiter
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
 
 limiter = Limiter(key_func=get_remote_address)
+
+
+def _rate_limit_exceeded_handler(request: Request, exc: RateLimitExceeded) -> Response:
+    """
+    Build a simple JSON response that includes the details of the rate limit
+    that was hit. If no limit is hit, the countdown is added to headers.
+    """
+    response = JSONResponse(
+        {
+            "error": "RateLimitExceeded",
+            "detail": f"Rate limit exceeded: {exc.detail}",
+        },
+        status_code=429,
+    )
+    return request.app.state.limiter._inject_headers(  # noqa: SLF001
+        response, request.state.view_rate_limit
+    )
 
 
 def setup(app: FastAPI) -> None:
